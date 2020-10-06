c36ea67e7fb0ac22914fa0f3e638687dcfac473b
rcreddyn <rcreddyn@icloud.com>
2020-10-07 00:03:35 +0530

    minor changes

--- testfits.py
+++ testfits.py
@@ -1,31 +1,43 @@
 import numpy as np #included in anaconda
 import matplotlib.pyplot as plt #included in anaconda
 from astropy.io import fits #install from pip
-from matplotlib.colors import LogNorm   
+from matplotlib.colors import LogNorm
 
 # refer to FITS file contents part from https://github.com/RCReddyN/radio-data-analysis
 
-def openandload(filename):  #returns "header data unit" object
+def retrievedata(filename):  #returns "header data unit" object
     hdu_list = fits.open(filename)
-    return hdu_list
+    image_data = hdu_list[0].data #image information is contained in primaryHDU
+    hdu_list.close()
+    return image_data
+
+def plot(img):
+    plt.imshow(img, cmap='gray')
+    plt.colorbar()
+    plt.savefig('orion.png')
+
+def disp_stats(img):
+    stats = np.array([np.min(img), np.max(img), np.mean(img), np.median(img), np.std(img)])
+    print("statistics:", stats)
+
+def plot_hist(img):
+    histogram = plt.hist(img.flatten(), bins = 'auto')
+    plt.savefig('orionhist.png')
+
+def plot_logcolors(img):
+    plt.imshow(img, cmap='gray', norm=LogNorm())
+    cbar = plt.colorbar(ticks=[5.e3,1.e4,2.e4])
+    cbar.ax.set_yticklabels(['5,000','10,000','19,000'])
+    plt.savefig('orion_logcolors.png')
 
 def main():
-    imagelocation = "./images/nebulae/dss.2.33.22+61.26.36.fits" #download fits file for respective coordinates
-    image = openandload(imagelocation) #assigns hdu_list object
-
-    #image.info() #displays information contained in the .fits file
-    image_data = image[0].data #image information is located in primary block
-
-    print(type(image_data)) #2D array containing the information of the image
-    print(image_data.shape) #dimensions of the image
-
-    image.close()   
-
-    plt.imshow(image_data, cmap='gray', norm=LogNorm())
-    cbar = plt.colorbar(ticks=[5.e3,1.e4,2.e4])
-    cbar.ax.set_yticklabels(['5,000','10,000','20,000'])
-    plt.colorbar()
-    plt.imsave('heart.png', image_data)
-
+    fitspath = "./images/nebulae/dss.5.35.17-5.23.28.fits" #download fits file for respective coordinates
+    image = retrievedata(fitspath) #assigns hdu_list object
+    print("dimensions:", image.shape)
+    #plot(image)
+    disp_stats(image)
+    #plot_hist(image)
+    plot_logcolors(image)
+     
 if __name__ == "__main__":
     main()    
